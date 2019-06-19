6be08765bb05825bccffe4afe96d8fd122e69310
AndrewLinscott <43798539+AndrewLinscott@users.noreply.github.com>
2019-06-19 01:45:23 -0400

    working onfixing books

--- CustomFunctionList.pas
+++ CustomFunctionList.pas
@@ -2882,62 +2882,68 @@ function BookTemplate(bookRecord:IInterface):IInterface;
 var
 	books, flags, tempSpellRecord: IInterface;
 	halfCostPerk: string;
+	A, C, D, I, R, School: char;
 begin
 	if (GetEditValue(elementbypath(selectedRecord, 'DATA\Flags\Teaches spell'))) = '1' then begin//checks if book is tome
 		tempSpellRecord := LinksTo(elementbypath(bookRecord, 'DATA\Flags\Teaches'));//spell from tome
-		if not (LinksTo(elementbypath(tempSpellRecord, 'SPIT\Half-cost Perk')) = nil) then begin
-			halfCostPerk := GetElementEditValues(tempSpellRecord, 'SPIT\Half-cost Perk');
-			{Debug} msg('halfCostPerk' + halfCostPerk);
+		halfCostPerk := GetElementEditValues(tempSpellRecord, 'SPIT - Data\Half-cost Perk');
+		if not (halfCostPerk = 'NULL - Null Reference [00000000]') then begin
+			A := 'A';
+			C := 'C';
+			D := 'D';
+			I := 'I';
+			R := 'R';
+			School := elementbypath(halfCostPerk, 'Novice', True);//get first char for all
 			case extractInts(halfCostPerk, 1) of
 			00	:	begin
-						case elementbypath(halfCostPerk, 'Novice', True) of
-							'Alteration'	:	Result :=GetRecordByFormID('0009E2A7');
-							'Conjuration'	:	Result :=GetRecordByFormID('0009E2AA');
-							'Destruction'	:	Result :=GetRecordByFormID('0009CD52');
-							'Illusion'		:	Result :=GetRecordByFormID('0009E2AD');
-							'Restoration'	:	Result :=GetRecordByFormID('0009E2AE');
+						case ord(School) of
+							ord(A){lteration}	:	Result :=GetRecordByFormID('0009E2A7');
+							ord(C){onjuration}	:	Result :=GetRecordByFormID('0009E2AA');
+							ord(D){estruction}	:	Result :=GetRecordByFormID('0009CD52');
+							ord(I){llusion}		:	Result :=GetRecordByFormID('0009E2AD');
+							ord(R){estoration}	:	Result :=GetRecordByFormID('0009E2AE');
 						end;
 					end;
 			25	:	begin
 						case elementbypath(halfCostPerk, 'Apprentice', True) of
-							'Alteration'	:	Result :=GetRecordByFormID('000A26E3');
-							'Conjuration'	:	Result :=GetRecordByFormID('0009CD54');
-							'Destruction'	:	Result :=GetRecordByFormID('000A2702');
-							'Illusion'		:	Result :=GetRecordByFormID('000A270F');
-							'Restoration'	:	Result :=GetRecordByFormID('000A2720');
+							ord(A){lteration}	:	Result :=GetRecordByFormID('000A26E3');
+							ord(C){onjuration}	:	Result :=GetRecordByFormID('0009CD54');
+							ord(D){estruction}	:	Result :=GetRecordByFormID('000A2702');
+							ord(I){llusion}		:	Result :=GetRecordByFormID('000A270F');
+							ord(R){estoration}	:	Result :=GetRecordByFormID('000A2720');
 						end;
 					end;
 			50	:	begin
 						case elementbypath(halfCostPerk, 'Adept', True) of
-							'Alteration'	:	Result :=GetRecordByFormID('000A26E7');
-							'Conjuration'	:	Result :=GetRecordByFormID('000A26EE');
-							'Destruction'	:	Result :=GetRecordByFormID('000A2708');
-							'Illusion'		:	Result :=GetRecordByFormID('000A2714');
-							'Restoration'	:	Result :=GetRecordByFormID('0010F64D');
+							ord(A){lteration}	:	Result :=GetRecordByFormID('000A26E7');
+							ord(C){onjuration}	:	Result :=GetRecordByFormID('000A26EE');
+							ord(D){estruction}	:	Result :=GetRecordByFormID('000A2708');
+							ord(I){llusion}		:	Result :=GetRecordByFormID('000A2714');
+							ord(R){estoration}	:	Result :=GetRecordByFormID('0010F64D');
 						end;
 					end;
 			75	:	begin
 						case elementbypath(halfCostPerk, 'Expert', True) of
-							'Alteration'	:	Result :=GetRecordByFormID('000A26E8');
-							'Conjuration'	:	Result :=GetRecordByFormID('000A26F7');
-							'Destruction'	:	Result :=GetRecordByFormID('0010F7F4');
-							'Illusion'		:	Result :=GetRecordByFormID('000A2718');
-							'Restoration'	:	Result :=GetRecordByFormID('000A2729');
+							ord(A){lteration}	:	Result :=GetRecordByFormID('000A26E8');
+							ord(C){onjuration}	:	Result :=GetRecordByFormID('000A26F7');
+							ord(D){estruction}	:	Result :=GetRecordByFormID('0010F7F4');
+							ord(I){llusion}		:	Result :=GetRecordByFormID('000A2718');
+							ord(R){estoration}	:	Result :=GetRecordByFormID('000A2729');
 						end;
 					end;
 			100	:	begin
 						case elementbypath(halfCostPerk, 'Master', True) of
-							'Alteration'	:	Result :=GetRecordByFormID('000DD646');
-							'Conjuration'	:	Result :=GetRecordByFormID('000A26FA');
-							'Destruction'	:	Result :=GetRecordByFormID('000A270D');
-							'Illusion'		:	Result :=GetRecordByFormID('000A2719');
-							'Restoration'	:	Result :=GetRecordByFormID('000FDE7B');
+							ord(A){lteration}	:	Result :=GetRecordByFormID('000DD646');
+							ord(C){onjuration}	:	Result :=GetRecordByFormID('000A26FA');
+							ord(D){estruction}	:	Result :=GetRecordByFormID('000A270D');
+							ord(I){llusion}		:	Result :=GetRecordByFormID('000A2719');
+							ord(R){estoration}	:	Result :=GetRecordByFormID('000FDE7B');
 						end;
 					end;
 			end;
 		end
 		else begin //uses restoration books as level list base
-			case StrToInt(GetElementEditValues(tempSpellRecord, 'SPIT/BASE COST')) of
+			case StrToInt(GetElementEditValues(tempSpellRecord, 'SPIT - Data\flags')) of //ght flag value
 				0..96		: Result :=GetRecordByFormID('0009E2AE');//novice
 				97..156		: Result :=GetRecordByFormID('000A2720');//aprentice
 				157..250	: Result :=GetRecordByFormID('0010F64D');//adept
